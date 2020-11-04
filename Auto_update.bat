dbd5598f0755064904da08ddd146bcb69a32f4d7
itsign-cybersecurity <73917231+itsign-cybersecurity@users.noreply.github.com>
2020-11-03 19:13:03 -0500

    Update Auto_update.bat

--- Auto_update.bat
+++ Auto_update.bat
@@ -1,6 +1,6 @@
 @echo on
 cd C:\Sysmon\
-@powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rothi83/sysmon-config/master/sysmonconfig-export.xml" -OutFile "C:\Sysmon\sysmonconfig-export.xml"
+@powershell Invoke-WebRequest -Uri "hhttps://raw.githubusercontent.com/itsign-cybersecurity/sysmon-config/main/sysmonconfig-export.xml" -OutFile "C:\Sysmon\sysmonconfig-export.xml"
 timeout 30
 sysmon64 -c sysmonconfig-export.xml
 exit
